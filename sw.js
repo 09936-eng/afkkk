9f438f07c8a96a0351051d35cdfada1e78bb149b
09936-eng <09936@khamphoempittaya.ac.th>
2026-08-20 11:03:28 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192×192.png',
-  './icon-512×512.png'
+  './icon-192.png',
+  './icon-512.png'
   // หากมีไฟล์ CSS/JS หรือรูปภาพอื่นๆ ให้ใส่ path เข้ามาที่นี่เพิ่ม เช่น './style.css', './script.js'
 ];
 
